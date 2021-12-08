bab2bce6bd4d8a8b58338ce3214c3cc951e40bb4
Antonio Cituk <tonycituk@gmail.com>
2021-12-08 13:33:13 -0500

    Initialize the project

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+/* 
+    @Test
+    public void testFraccionSimplificada() {
+        Fraccion fraccionA = new Fraccion(8, 4); // 8/4 => 2/1
+        Fraccion fraccionB = new Fraccion(2); // 2/1 => already simplified
+
+        assertTrue((fraccionA.getNum() == fraccionB.getNum()) && (fraccionA.getDen() == fraccionB.getDen()));
+    } */
+}
